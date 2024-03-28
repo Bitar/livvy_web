2dad523199b824a8297ca212730f6f1900acafcc
Ayman Bitar <ayman@livvy.com>
2024-03-28 11:32:59 -0400

    onboarding

--- src/sections/onboarding/pages/Onboarding.tsx
+++ src/sections/onboarding/pages/Onboarding.tsx
@@ -1,7 +1,12 @@
 export const Onboarding = () => {
     return (
         <>
-
+            <div id='onboarding' className='text-white'>
+                <p>Step 01</p>
+                <h1>Capture <i>Your Space</i></h1>
+                <p>Creating a digital twin is the first step towards converting your inspiration into a shoppable reality with LIVVY.</p>
+            </div>
         </>
     )
 }
+
