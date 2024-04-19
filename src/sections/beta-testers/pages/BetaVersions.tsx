a64bd533782356c6ed58f89cde3b31f9ae103cd2
Hadil Charafeddine <hadil@livvy.com>
2024-04-19 10:18:58 -0400

    removed pading from versions list on desktop

--- src/sections/beta-testers/pages/BetaVersions.tsx
+++ src/sections/beta-testers/pages/BetaVersions.tsx
@@ -73,7 +73,7 @@ export const BetaVersions = () => {
                 </div>
             </div>
 
-            <div className="container py-7 px-4">
+            <div className="container py-7 px-4 md:px-0">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
                     {
                         appVersions.map((appVersion, idx) => <VideoPreview
