efb331bec6ea4d9684d6a631a3b5a65d666cbce7
Ayman Bitar <ayman@livvy.com>
2024-04-30 09:56:16 -0400

    Moved CTA from 'Explore Details' to the whole AppVersion Card

--- src/sections/beta-testers/pages/AlphaVersions.tsx
+++ src/sections/beta-testers/pages/AlphaVersions.tsx
@@ -53,35 +53,35 @@ export const AppVersionCard = ({appVersion, isHero, isThumbnail = false}: { appV
             "lg:aspect-square": !isHero && !isThumbnail,
             "lg:pt-[56.25%]": isHero && !isThumbnail
         })}>
-            <div className="overlay absolute w-full h-full bg-black opacity-40 top-0 left-0"></div>
-            <div className="overlay absolute w-full h-full bg-gradient-to-b from-transparent from-30% to-black top-0 left-0"></div>
-            <div className={clsx("mb-10 mt-10 px-10 relative uppercase", {
-                'lg:absolute lg:top-10': !isThumbnail
-            })}>
-                {
-                    isHero &&
-                    <span className='rounded-full bg-white py-4 px-5 sm:text-sm text-xs me-4'>New Release</span>
-                }
-                <span className='text-white sm:text-sm text-xs'>{moment.unix(appVersion.created_at).format('MMM DD, YYYY')}</span>
-            </div>
-            <div className={clsx("content relative mb-10 px-10 text-white", {
-                "lg:absolute lg:bottom-10 ": !isThumbnail
-            })}>
-                <h1 className={clsx("title sm:text-4xl text-3xl uppercase mb-5", {
-                    'lg:text-6xl sm:text-4xl text-3xl': !isThumbnail,
-                    'sm:text-4xl text-3xl': isThumbnail
-                })}>Version ({appVersion.version})</h1>
-                <div
-                    className={clsx("description mb-5", {
-                        "max-w-2xl": isHero
-                    })}>
-
-                    {appVersion.description?.substring(0, 250)}{appVersion.description.length > 250 && "..."}
+            <Link to={`/alpha/${appVersion.slug}`}>
+                <div className="overlay absolute w-full h-full bg-black opacity-40 top-0 left-0"></div>
+                <div className="overlay absolute w-full h-full bg-gradient-to-b from-transparent from-30% to-black top-0 left-0"></div>
+                <div className={clsx("mb-10 mt-10 px-10 relative uppercase", {
+                    'lg:absolute lg:top-10': !isThumbnail
+                })}>
+                    {
+                        isHero &&
+                        <span className='rounded-full bg-white py-4 px-5 sm:text-sm text-xs me-4'>New Release</span>
+                    }
+                    <span className='text-white sm:text-sm text-xs'>{moment.unix(appVersion.created_at).format('MMM DD, YYYY')}</span>
                 </div>
-                <Link to={`/alpha/${appVersion.slug}`} className='inline-block'>
+                <div className={clsx("content relative mb-10 px-10 text-white", {
+                    "lg:absolute lg:bottom-10 ": !isThumbnail
+                })}>
+                    <h1 className={clsx("title sm:text-4xl text-3xl uppercase mb-5", {
+                        'lg:text-6xl sm:text-4xl text-3xl': !isThumbnail,
+                        'sm:text-4xl text-3xl': isThumbnail
+                    })}>Version ({appVersion.version})</h1>
+                    <div
+                        className={clsx("description mb-5", {
+                            "max-w-2xl": isHero
+                        })}>
+
+                        {appVersion.description?.substring(0, 250)}{appVersion.description.length > 250 && "..."}
+                    </div>
                     <span style={{fontFamily: "PP Editorial New"}} className='cta italic border-b font-thin sm:text-2xl text-xl'>Explore Details</span>
-                </Link>
-            </div>
+                </div>
+            </Link>
         </div>
     )
 }
