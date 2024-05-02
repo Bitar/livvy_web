6fa2a4b3c98b6478842149589b53dc36600ff617
Hadil Charafeddine <hadil@Hadils-MacBook-Pro.local>
2024-05-02 18:45:06 -0400

    fixed the checkmark size for checklist in membership page

--- src/sections/membership/pages/Membership.tsx
+++ src/sections/membership/pages/Membership.tsx
@@ -12,13 +12,13 @@ export const Membership = () => {
 
     return (
         <div className="container liv-container">
-            <div className="flex justify-between items-center sm:mb-20 mb-8">
+            <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center sm:mb-20 mb-8">
                 <h1 className={'text-4xl md:text-5xl lg:text-7xl uppercase'}>select your <br/> <span style={{fontFamily: "PP Editorial New"}}
                                                                                                      className="font-thin italic capitalize">membership</span>
                 </h1>
 
-                <div className="bg-black rounded-full w-44 h-44 lg:w-48 lg:h-48 relative hidden sm:inline-block">
-                    <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-xs text-center w-3/5">
+                <div className="sm:bg-black sm:rounded-full sm:w-44 sm:h-44 lg:w-48 lg:h-48 relative sm:inline-block">
+                    <p className="sm:absolute sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 sm:text-white text-xs sm:text-center sm:w-3/5">
                         Hiring an interior designer for a single room typically costs <span className="font-bold">$1,500
                         to $12,000</span>. <br/> At LIVVY we make designing your dream space accessible.
                     </p>
@@ -51,7 +51,7 @@ const Plan = ({name, perMonth, perYear, perks}: { name: string, perMonth: string
             <ul className="py-5 sm:py-7">
                 {
                     perks.map((perk, index) => <li className="flex justify-start items-start capitalize mb-6" key={index}>
-                        <span className="me-2"><img src={`/assets/check.svg`} className="w-[20px] h-[20px]" alt={"checkmark"}/></span>
+                        <img src={`/assets/check.svg`} className="me-2 w-[20px] h-[20px]" alt={"checkmark"}/>
                         <span>{perk}</span>
                     </li>)
                 }
