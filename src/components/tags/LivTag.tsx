4a30da5da829b680c1473708a1c5977d25fd2b7f
Ayman Bitar <ayman@livvy.com>
2024-05-06 18:05:17 -0400

    feedback

--- src/components/tags/LivTag.tsx
+++ src/components/tags/LivTag.tsx
@@ -9,6 +9,6 @@ export const LivTag: FC<LivTagProps> = (props) => {
     const {backgroundColor, text} = props
 
     return (
-        <span className={`flex items-center justify-center uppercase px-2 py-1 lg:px-3 lg:py-2 min-w-24 rounded-full border border-black text-xs ${backgroundColor}`}>{text}</span>
+        <span className={`flex items-center justify-center uppercase px-1 py-1 lg:px-3 lg:py-2 min-w-24 rounded-full border border-black text-2xs ${backgroundColor}`}>{text}</span>
     )
 }
