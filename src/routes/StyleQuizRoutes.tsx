8808a45ce167e8811de75cf7f184bb5049d04742
Hadil Charafeddine <hadil@Hadils-MacBook-Pro.local>
2024-05-24 09:58:14 +0200

    done with results page for style quiz

--- src/routes/StyleQuizRoutes.tsx
+++ src/routes/StyleQuizRoutes.tsx
@@ -2,6 +2,7 @@ import {Route, Routes} from "react-router-dom";
 
 import {TakeQuiz} from "../sections/style-quiz/pages/TakeQuiz.tsx";
 import {Quiz} from "../sections/style-quiz/pages/Quiz.tsx";
+import {Results} from "../sections/style-quiz/pages/Results.tsx";
 
 export const StyleQuizRoutes = () => {
     return (
@@ -9,6 +10,8 @@ export const StyleQuizRoutes = () => {
             <Route index element={<TakeQuiz/>}/>
 
             <Route path={'/quiz'} element={<Quiz/>}/>
+
+            <Route path={'/results'} element={<Results/>}/>
         </Routes>
     )
 }
